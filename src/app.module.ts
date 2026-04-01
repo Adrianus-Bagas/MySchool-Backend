9a9304ba8a953e0d6a8e57c64d1a3bdefb612f17
Adrianus-Bagas <bagasadrianus80@gmail.com>
2026-04-01 10:04:42 +0700

    chore: setup graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+    }),
+    PrismaModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
